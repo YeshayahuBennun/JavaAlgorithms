3805efd69d7352ffe73f88d79c56bc1cfe0117ec
YeshayahuBennun <ybennun@yahoo.com>
2022-03-31 20:00:02 +0300

    commit#0 - Creation - Find The Sum And Product Of Array

---
+++ src/arrays/FindTheSumAndProductOfArray.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class FindTheSumAndProductOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,5};
+
+        spOfArray(arr);
+    }
+
+    private static void spOfArray(int[] arr) {
+        int sum = 0;                          //O(1)
+        int product = 1;                      //O(1)
+
+        for (int i = 0; i < arr.length; i++) {//O(n)
+            sum += arr[i];//O(1)
+        }                                                       //=> O(n) complexity
+
+        for (int i = 0; i < arr.length; i++) {//O(n)
+            product *= arr[i];
+        }
+
+        System.out.println(sum + "," + product);//O(1)
+    }
+}
